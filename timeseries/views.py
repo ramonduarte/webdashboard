fb4c6dec2c7be004314d404bdef06094aa59d94b
ramonduarte <ramonduarte@poli.ufrj.br>
2017-06-17 20:49:26 -0300

    Bokeh application does not bode well in Django's architecture, so it was moved to another application: https://github.com/ramonduarte/bokeh-dashboard

--- timeseries/views.py
+++ timeseries/views.py
@@ -1,18 +1,4 @@
 from django.shortcuts import render, Http404
-from bokeh.layouts import column
-from bokeh.models import Button, ColumnDataSource, HoverTool
-from bokeh.models.glyphs import VBar
-from bokeh.models.widgets import Slider, Select
-from bokeh.palettes import RdYlBu3
-from bokeh.layouts import layout, widgetbox
-from bokeh.plotting import figure, curdoc, show, output_file
-from bokeh.embed import file_html, components
-from bokeh.resources import CDN
-from bokeh.client import push_session
-
-import pandas as pd
-import os
-from calendar import monthrange
 
 
 # from webdashboard import settings
@@ -21,8 +7,8 @@ def home(request):
     if request.method == 'GET':
         context = {
             'bokeh_app_src': 'http://localhost:5006/dashboard',
-            'bokeh_app_width': 1024,
-            'bokeh_app_height': 800,
+            'bokeh_app_width': 800,
+            'bokeh_app_height': 400,
         }
         return render(request, 'timeseries/index.html', context=context)
     else:
