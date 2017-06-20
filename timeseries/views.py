d4808e31335347d04d2c1dbb8ad87fab805aad55
ramonduarte <ramonduarte@poli.ufrj.br>
2017-06-20 11:46:10 -0300

    Fix bokeh-dashboard url typo.

--- timeseries/views.py
+++ timeseries/views.py
@@ -7,7 +7,7 @@ from datetime import datetime
 def home(request):
     if request.method == 'GET':
         context = {
-            'bokeh_app_src': 'http://http://104.236.5.21:5006/sources',
+            'bokeh_app_src': 'http://104.236.5.21:5006/sources',
             'bokeh_app_width': 800,
             'bokeh_app_height': 800,
         }
